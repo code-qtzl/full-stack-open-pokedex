e2d838c690156cdcb1fee353c616751c8dbf2a6c
code-qtzl <azepeda90@outlook.com>
2025-12-12 16:17:09 -0600

    part11: exercise 11.9

--- tests/pokedex.spec.js
+++ tests/pokedex.spec.js
@@ -2,7 +2,7 @@ import { test, describe, expect } from '@playwright/test'
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080/')
+    await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(
       page.getByText(
@@ -12,13 +12,13 @@ describe('Pokedex', () => {
   })
 
   test('can navigate to a pokemon page', async ({ page }) => {
-    await page.goto('http://localhost:8080/')
+    await page.goto('/')
     await page.getByText('ivysaur').click()
     await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 
   test('can scroll and see more pokemon in the list', async ({ page }) => {
-    await page.goto('http://localhost:8080/')
+    await page.goto('/')
     await expect(page.getByText('charmeleon')).toBeVisible()
     await expect(page.getByText('pikachu')).toBeVisible()
   })
