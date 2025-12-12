da0f411bebf933055004194919e9bcad14007d1c
code-qtzl <azepeda90@outlook.com>
2025-12-12 15:41:10 -0600

    part11: exercise 11.9

---
+++ tests/pokedex.spec.js
@@ -0,0 +1,25 @@
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.',
+      ),
+    ).toBeVisible()
+  })
+
+  test('can navigate to a pokemon page', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+
+  test('can scroll and see more pokemon in the list', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await expect(page.getByText('charmeleon')).toBeVisible()
+    await expect(page.getByText('pikachu')).toBeVisible()
+  })
+})
