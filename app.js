931199c301d17c9eecffe006c2a3edfde85f04f4
code-qtzl <azepeda90@outlook.com>
2025-12-16 16:38:08 -0600

    part11: exercise 11.10-11.11

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = 3001
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  // server started on port ${PORT}
+  // eslint-disable-next-line no-console
+  console.log(`Server started successfully at http://localhost:${PORT}`)
 })
