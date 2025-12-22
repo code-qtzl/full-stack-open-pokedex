d01064d40f86c6356aab778065f093503bc0833c
code-qtzl <azepeda90@outlook.com>
2025-12-21 23:36:31 -0600

    part11: exercise 11.19

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
-  console.log(`Server started successfully at http://localhost:${PORT}`)
+  console.log(`Server started successfully on 0.0.0.0:${PORT}`)
 })
